66b76e0bbee144cdda7e54715bf5d4d8ee62eca9
Zia Ur Rehman <ziaa520@gmail.com>
2021-05-28 23:29:05 +0500

    ss

---
+++ app/Controllers/Login.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Controllers;
+use App\Models\Auth;
+use App\Models\LoginModal;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+class Login extends ResourceController
+{
+    use ResponseTrait;
+    protected $modelName = 'App\Models\Login';
+    protected $format    = 'json';
+    private $auth;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $model = new LoginModal();
+
+        $data = [
+            'news'  => $model->getData(),
+            'title' => 'News archive',
+        ];
+
+        return $data;
+    }
+
+    public function create()
+    {
+
+       
+        $post = file_get_contents("php://input");
+        $POST  =json_decode($post, true);
+        $pass = isset($POST['user_passowrd']) ? $POST['user_passowrd'] : '';
+        $login = isset($POST['user_email']) ? $POST['user_email'] : '';
+       // return $this->fail( $POST, 400);
+        if(!$pass || !$login){
+            return $this->fail("Missing user_passowrd or user_email", 400);
+        }
+        $model = new LoginModal();
+        $data['token']  = $model->do_login($login,$pass);
+
+
+        if($data['token']){
+            return $this->respondCreated($data);
+            return $this->respond($data['token'], 200);
+        }
+
+        return $this->fail('Access denied', 401,'TYU7890');
+        // $data = [
+        //     'news'  => 'ok',
+        //     'title' => 'News archive',
+        // ];
+        // return $this->respond($data, 200);
+        // return $this->respondCreated();
+        // return $data;
+        // return view('login/login', $this->model->findAll());
+        // return $this->respond($this->model->findAll());
+    }
+
+    // ...
+}
---
+++ public/index.php
@@ -0,0 +1,62 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");         
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+// Valid PHP Version?
+$minPHPVersion = '7.3';
+if (version_compare(PHP_VERSION, $minPHPVersion, '<'))
+{
+	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . PHP_VERSION);
+}
+unset($minPHPVersion);
+
+// Path to the front controller (this file)
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+ini_set('display_errors', 1);
+/*
+ *---------------------------------------------------------------
+ * BOOTSTRAP THE APPLICATION
+ *---------------------------------------------------------------
+ * This process sets up the path constants, loads and registers
+ * our autoloader, along with Composer's, loads our constants
+ * and fires up an environment-specific bootstrapping.
+ */
+
+// Ensure the current directory is pointing to the front controller's directory
+chdir(__DIR__);
+
+// Load our paths config file
+// This is the line that might need to be changed, depending on your folder structure.
+require realpath(FCPATH . '../app/Config/Paths.php') ?: FCPATH . '../app/Config/Paths.php';
+// ^^^ Change this if you move your application folder
+
+$paths = new Config\Paths();
+
+// Location of the framework bootstrap file.
+$bootstrap = rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+$app       = require realpath($bootstrap) ?: $bootstrap;
+
+/*
+ *---------------------------------------------------------------
+ * LAUNCH THE APPLICATION
+ *---------------------------------------------------------------
+ * Now that everything is setup, it's time to actually fire
+ * up the engines and make this app do its thang.
+ */
+$app->run();
